160c90e16b50e9d84774a054655ba914a566f2c3
swadmahfuz <swad.mahfuz@gmail.com>
2024-05-25 15:46:37 +0600

    Fixed bug where deleted certificates were not shown properly.
    Fixed bug where deleted certificates were not shown properly after implementing live search. Created a separate page for deleted certificates.

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -68,7 +68,7 @@ class CertificateController extends Controller
         if (Auth::check())
         {
             $certificates = Certificate::onlyTrashed()->orderBy('inspection_date','DESC')->paginate(100);
-            return view('dashboard',compact('certificates'));
+            return view('deleted-certificates',compact('certificates'));
         }
 
         return redirect('/admin');
